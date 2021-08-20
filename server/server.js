f4e4c0d8e04486e26d2c7bc4238c44cb94f06133
Zyo86 <zyothebest86@gmail.com>
2021-08-20 20:02:08 +0530

    SSR server added using express

--- server/server.js
+++ server/server.js
@@ -12,3 +12,35 @@ const PORT = 8000;
 const app = express();
 
 const router = app.router();
+
+app.use('./build', express.static('build'));
+
+app.get('*', (req, res) => {
+  const context = {};
+  const app = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+
+  const indexFile = path.resolve('./build/index.html');
+  fs.readFile(indexFile, 'utf-8', (err, data) => {
+    if (err) {
+      console.log('Error');
+      return res.status(500).send('oops !', 'error');
+    }
+    return res.send(
+      data.replace(`<div id="root"></div>`, `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+route.use(
+  express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '10d' })
+);
+
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log('Listening to SSR on port : ' + PORT);
+});
